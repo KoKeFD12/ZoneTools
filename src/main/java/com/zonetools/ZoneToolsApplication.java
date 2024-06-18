950c4d6eb73ce1bd582d90b15b3198e145ac1471
KoKeFD12 <jfcomms.contact@gmail.com>
2024-06-18 16:40:25 +0200

    Initial commit.

---
+++ src/main/java/com/zonetools/ZoneToolsApplication.java
@@ -0,0 +1,13 @@
+package com.zonetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoneToolsApplication.class, args);
+	}
+
+}
