04c588456e445f6bfab4107f27237b04cf4312e4
KoKeFD12 <jfcomms.contact@gmail.com>
2024-06-18 18:01:59 +0200

    MovementModel.java creation.

---
+++ src/main/java/com/zonetools/models/MovementModel.java
@@ -0,0 +1,37 @@
+package com.zonetools.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "movements")
+public class MovementModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private char type;
+
+    @Column(name = "territory_id")
+    private int territoryId;
+
+    private String name;
+    private LocalDate date;
+
+}
