54023c534f9a71b61f599ea698cf60ee68a34fac
KoKeFD12 <jfcomms.contact@gmail.com>
2024-06-18 22:39:52 +0200

    Changed "id" from int to Long

--- src/main/java/com/zonetools/models/MovementModel.java
+++ src/main/java/com/zonetools/models/MovementModel.java
@@ -24,12 +24,12 @@ public class MovementModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private char type;
 
     @Column(name = "territory_id")
-    private int territoryId;
+    private Long territoryId;
 
     private String owner;
     private LocalDate date;
