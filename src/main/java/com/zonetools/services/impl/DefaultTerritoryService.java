4745dcd1a949ec98c757f37886e4b2bf21059dc7
KoKeFD12 <jfcomms.contact@gmail.com>
2024-06-18 22:40:45 +0200

    Created the DefaultTerritoryService.java

--- src/main/java/com/zonetools/services/impl/DefaultTerritoryService.java
+++ src/main/java/com/zonetools/services/impl/DefaultTerritoryService.java
@@ -1,61 +1,74 @@
 package com.zonetools.services.impl;
 
+import com.zonetools.daos.MovementDao;
+import com.zonetools.daos.TerritoryDao;
 import com.zonetools.models.MovementModel;
 import com.zonetools.models.TerritoryModel;
 import com.zonetools.services.TerritoryService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
+@Service
 public class DefaultTerritoryService implements TerritoryService {
+
+    private final MovementDao movementDao;
+    private final TerritoryDao territoryDao;
+
     @Override
     public List<TerritoryModel> findAllTerritories() {
-        return List.of();
+        return territoryDao.findAll();
     }
 
     @Override
     public List<TerritoryModel> findTerritoriesByOwner(String owner) {
-        return List.of();
+        return territoryDao.findAllByOwnerContaining(owner);
     }
 
     @Override
     public Optional<TerritoryModel> findTerritoryById(Long id) {
-        return Optional.empty();
+        return territoryDao.findById(id);
     }
 
     @Override
     public TerritoryModel saveTerritory(TerritoryModel territory) {
-        return null;
+        return territoryDao.save(territory);
     }
 
     @Override
     public List<MovementModel> findAllMovements() {
-        return List.of();
+        return movementDao.findAll();
     }
 
     @Override
     public Optional<MovementModel> findMovementById(Long id) {
-        return Optional.empty();
+        return movementDao.findById(id);
     }
 
     @Override
-    public Optional<MovementModel> findMovementByDate(LocalDate date) {
-        return Optional.empty();
+    public List<MovementModel> findMovementsByDate(LocalDate date) {
+        return movementDao.findAllByDate(date);
     }
 
     @Override
     public List<MovementModel> findMovementsByTerritoryID(Long id) {
-        return List.of();
+        return movementDao.findAllByTerritoryId(id);
     }
 
     @Override
     public List<MovementModel> findMovementsByOwner(String owner) {
-        return List.of();
+        return movementDao.findAllByOwnerContaining(owner);
     }
 
     @Override
     public MovementModel saveMovement(MovementModel movementModel) {
-        return null;
+        return movementDao.save(movementModel);
     }
+
 }
