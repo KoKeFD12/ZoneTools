bf221b5f2d099e7fec2678381274bab15d527fd9
KoKeFD12 <jfcomms.contact@gmail.com>
2024-06-18 18:19:04 +0200

    Added custom methods

--- src/main/java/com/zonetools/daos/MovementDao.java
+++ src/main/java/com/zonetools/daos/MovementDao.java
@@ -3,6 +3,11 @@ package com.zonetools.daos;
 import com.zonetools.models.MovementModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MovementDao extends JpaRepository<MovementModel, Long> {
+
+    List<MovementModel> findAllByDate(LocalDate date);
 
 }
