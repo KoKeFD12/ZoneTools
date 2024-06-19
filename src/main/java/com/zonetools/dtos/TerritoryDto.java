67c7c51993422169a24f910dacc0be4171e995a9
KoKeFD12 <jfcomms.contact@gmail.com>
2024-06-19 14:31:38 +0200

    DTOs creation

---
+++ src/main/java/com/zonetools/dtos/TerritoryDto.java
@@ -0,0 +1,26 @@
+package com.zonetools.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class TerritoryDto {
+
+    private Long id;
+
+    private int doors;
+
+    private String imageUrl;
+
+    private String owner;
+
+    private LocalDate lastAssigned;
+
+    private LocalDate lastReturned;
+
+}
