809ee20483a857cce8c972ee6ef44aa36436e716
KoKeFD12 <jfcomms.contact@gmail.com>
2024-06-22 20:07:43 +0200

    DefaultMovementServiceTest.java creation

---
+++ src/test/java/com/zonetools/services/impl/DefaultMovementServiceTest.java
@@ -0,0 +1,46 @@
+package com.zonetools.services.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultMovementServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAllMovements() {
+    }
+
+    @Test
+    void findMovementById() {
+    }
+
+    @Test
+    void findMovementsByDate() {
+    }
+
+    @Test
+    void findMovementsByTerritoryID() {
+    }
+
+    @Test
+    void findMovementsByOwner() {
+    }
+
+    @Test
+    void saveMovement() {
+    }
+
+    @Test
+    void deleteMovement() {
+    }
+}
