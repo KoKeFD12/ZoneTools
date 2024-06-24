26fd7fec1c7b41bb4b1067721f1c83c0552c569b
KoKeFD12 <jfcomms.contact@gmail.com>
2024-06-24 17:59:49 +0200

    Thymeleaf configuration and home creation

--- src/main/java/com/zonetools/controllers/TerritoryController.java
+++ src/main/java/com/zonetools/controllers/TerritoryController.java
@@ -3,11 +3,18 @@ package com.zonetools.controllers;
 import com.zonetools.facades.TerritoryFacade;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TerritoryController {
 
     @Resource
     private TerritoryFacade territoryFacade;
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        return "home";
+    }
 
 }
