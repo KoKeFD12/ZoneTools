e0b232884311293142a8d5905061cb2731a861e5
KoKeFD12 <jfcomms.contact@gmail.com>
2024-07-28 16:36:31 +0200

    Added mappings

--- src/main/java/com/zonetools/controllers/TerritoryController.java
+++ src/main/java/com/zonetools/controllers/TerritoryController.java
@@ -4,7 +4,7 @@ import com.zonetools.facades.TerritoryFacade;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TerritoryController {
@@ -12,9 +12,20 @@ public class TerritoryController {
     @Resource
     private TerritoryFacade territoryFacade;
 
-    @RequestMapping("/home")
+    @GetMapping("/")
     public String home(Model model) {
         return "home";
+    }
+
+    @GetMapping("/movements")
+    public String movements(Model model) {
+        return "movements";
+    }
+
+    @GetMapping("/territories")
+    public String territories(Model model) {
+        model.addAttribute("territoriesList", territoryFacade.findAllTerritories());
+        return "territories";
     }
 
 }
