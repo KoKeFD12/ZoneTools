f9cf9a56b6fdf147bb7aa68b6e026500b7e8b319
KoKeFD12 <jfcomms.contact@gmail.com>
2024-07-28 16:57:31 +0200

    Changed names to constants

--- src/main/java/com/zonetools/controllers/TerritoryController.java
+++ src/main/java/com/zonetools/controllers/TerritoryController.java
@@ -9,28 +9,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TerritoryController {
 
+    private static final String HOME_PAGE = "home";
+    private static final String MOVEMENTS_PAGE = "movements";
+    private static final String TERRITORIES_PAGE = "territories";
+    private static final String NEW_TERRITORY_PAGE = "new-territory";
+
     @Resource
     private TerritoryFacade territoryFacade;
 
     @GetMapping("/")
     public String home(Model model) {
-        return "home";
+        return HOME_PAGE;
     }
 
     @GetMapping("/movements")
     public String movements(Model model) {
-        return "movements";
+        return MOVEMENTS_PAGE;
     }
 
     @GetMapping("/territories")
     public String territories(Model model) {
         model.addAttribute("territoriesList", territoryFacade.findAllTerritories());
-        return "territories";
+        return TERRITORIES_PAGE;
     }
 
     @GetMapping("/territories/new-territory")
     public String newTerritory(Model model) {
-        return "new-territory";
+        return NEW_TERRITORY_PAGE;
     }
 
 }
